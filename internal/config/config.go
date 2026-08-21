280466f49a63f68c746bc59788923c93d1489115
markyyys11 <markyyys11@gmail.com>
2026-08-21 17:20:46 +0300

    feat: config expansion

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,6 +11,21 @@ import (
 type Config struct {
 	Release       bool
 	ServerAddress string
+	ClientAddress string
+	Token         tokenConfig
+}
+
+type cookieConfig struct {
+	Name     string
+	MaxAge   int
+	Path     string
+	Secure   bool
+	HttpOnly bool
+}
+
+type tokenConfig struct {
+	Secret string
+	Cookie cookieConfig
 }
 
 func NewConfig() Config {
@@ -25,13 +40,21 @@ func NewConfig() Config {
 	return Config{
 		Release:       release,
 		ServerAddress: getString("SERVER_ADDR", ":8080"),
+		Token: tokenConfig{
+			Secret: getString("TOKEN_SECRET", "test_secret"),
+			Cookie: cookieConfig{
+				Name:     getString("TOKEN_COOKIE_NAME", "refresh_token"),
+				MaxAge:   int(getInteger("TOKEN_COOKIE_MAX_AGE", 10000)),
+				Path:     getString("TOKEN_COOKIE_PATH", "refresh_token"),
+				Secure:   getBoolean("TOKEN_COOKIE_SECURE", false),
+				HttpOnly: getBoolean("TOKEN_COOKIE_HTTP_ONLY", true),
+			},
+		},
 	}
 }
 
 func getBoolean(key string, def bool) bool {
-	v, ex := os.LookupEnv(key)
-
-	if ex {
+	if v, exists := os.LookupEnv(key); exists {
 		if res, err := strconv.ParseBool(v); err == nil {
 			return res
 		}
@@ -41,10 +64,19 @@ func getBoolean(key string, def bool) bool {
 }
 
 func getString(key string, def string) string {
-	v, ex := os.LookupEnv(key)
-
-	if ex {
+	if v, exists := os.LookupEnv(key); exists {
 		return v
+	}
+
+	return def
+}
+
+func getInteger(key string, def int64) int64 {
+
+	if v, exists := os.LookupEnv(key); exists {
+		if i, err := strconv.ParseInt(v, 10, 64); err == nil {
+			return i
+		}
 	}
 
 	return def
